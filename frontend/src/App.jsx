76a403ff8c020c6c4c88880748a4fd73b440293a
Shelley McHardy <shelleymcq.dev@gmail.com>
2023-05-04 15:07:02 -0400

    login, signup, and user dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,13 @@
+import { AuthProvider } from "./Context/AuthContext";
 import "./App.css";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>Let's Eat</h1>
-        <Login />
-        <Signup />
-      </div>
-    </>
+    <AuthProvider>
+      <h1>Let's Eat</h1>
+      <Dashboard />
+    </AuthProvider>
   );
 }
 
---
+++ frontend/src/components/OutingDetails.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OutingDetails = () => {
+  return (
+    <div>
+      This will display all the details of a particular outing from the outing
+      object...in a popup?
+    </div>
+  );
+};
+
+export default OutingDetails;
