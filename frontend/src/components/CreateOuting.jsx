5b6296cdb5c24c3a6f00e9fbfebb799acf72c0f2
Shelley McHardy <shelleymcq.dev@gmail.com>
2023-06-05 12:22:01 -0400

    added existing user note to outing page

--- frontend/src/components/CreateOuting.jsx
+++ frontend/src/components/CreateOuting.jsx
@@ -1,14 +1,12 @@
-import { Stack } from '@mui/material'
-import React, { useState } from 'react'
-import dayjs from 'dayjs';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-import TextField from '@mui/material/TextField';
-import {
-  Button,
-} from "@mui/material";
+import { Stack } from "@mui/material";
+import React, { useState } from "react";
+import dayjs from "dayjs";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
 
 const CreateOuting = ({ createOuting }) => {
   const [datetime, setDatetime] = useState(dayjs());
@@ -16,21 +14,28 @@ const CreateOuting = ({ createOuting }) => {
   const [location, setLocation] = useState("");
 
   const setOutingDate = (newDate) => {
-    let newDatetime = dayjs(datetime).set('year', newDate.get('year'))
-                                     .set('month', newDate.get('month'))
-                                     .set('date', newDate.get('date'));
+    let newDatetime = dayjs(datetime)
+      .set("year", newDate.get("year"))
+      .set("month", newDate.get("month"))
+      .set("date", newDate.get("date"));
     setDatetime(newDatetime);
-  }
+  };
 
   const setOutingTime = (newTime) => {
-    let newDatetime = dayjs(datetime).set('hour', newTime.get('hour'))
-                                     .set('minute', newTime.get('minute'));
+    let newDatetime = dayjs(datetime)
+      .set("hour", newTime.get("hour"))
+      .set("minute", newTime.get("minute"));
     setDatetime(newDatetime);
-  }
+  };
 
   return (
-    <div className='createOuting'>
+    <div className="createOuting">
       <h1>Create a New Outing</h1>
+      <p>
+        You can only invite an existing user. Make sure your invitee has created
+        an acount and shared their username with you.
+      </p>
+
       <Stack
         sx={{ marginBottom: "20px" }}
         direction={{ xs: "column", sm: "row" }}
@@ -45,31 +50,31 @@ const CreateOuting = ({ createOuting }) => {
           justifyContent={"space-between"}
         >
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker 
+            <DatePicker
               label="Select Date"
               color="error"
               onChange={(newValue) => setOutingDate(newValue)}
               required={true}
             />
-            <TimePicker 
-              label="Select Time" 
+            <TimePicker
+              label="Select Time"
               onChange={(newValue) => setOutingTime(newValue)}
               format="hh:mm a"
               required={true}
             />
           </LocalizationProvider>
-          <TextField 
-            id="filled-basic" 
-            label="Username to Invite" 
-            variant="filled" 
+          <TextField
+            id="filled-basic"
+            label="Username to Invite"
+            variant="filled"
             required={true}
             onChange={(e) => setInvited(e.target.value)}
           />
-          <TextField 
-            id="filled-basic" 
-            label="Location... Type an address" 
-            placeholder='New York City'
-            variant="filled" 
+          <TextField
+            id="filled-basic"
+            label="Location... Type an address"
+            placeholder="New York City"
+            variant="filled"
             required={true}
             onChange={(e) => setLocation(e.target.value)}
           />
@@ -82,7 +87,7 @@ const CreateOuting = ({ createOuting }) => {
         </Button>
       </Stack>
     </div>
-  )
-}
+  );
+};
 
-export default CreateOuting
+export default CreateOuting;
